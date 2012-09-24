4ae306785800103b4c908ff80c26b4c18d7331e1
Nathan Hopkins <natehop@gmail.com>
2012-09-24 10:20:03 -0600

    Fixed the example.

--- lib/micro_mock.rb
+++ lib/micro_mock.rb
@@ -12,7 +12,7 @@
 #   Mock = MicroMock.make
 #
 #   # mock a class method
-#   Mock.stub :foo { :bar }
+#   Mock.stub(:foo) { :bar }
 #
 #   # make assertions
 #   assert_equal :bar, Mock.foo # Test::Unit
@@ -20,21 +20,21 @@
 #
 #   # mock an instance method
 #   m = Mock.new
-#   m.stub :bar { :foo }
+#   m.stub(:bar) { :foo }
 #
 #   # make assertions
 #   assert_equal :foo, m.bar # Test::Unit
 #   m.bar.should eq :foo # RSpec
 #
 #   # setup mock internal behavior
-#   m.stub :count { @count ||= 0 }
-#   m.stub :a { count += 1 }
-#   m.stub :b { |i| self.a if i > 5 }
+#   count = 1
+#   m.stub(:a) { count += 1 }
+#   m.stub(:b) { |i| self.a if i > 5 }
 #
 #   # make assertions
-#   10.times { |i| m.b }
-#   assert_equal 5, m.count # Test::Unit
-#   m.count.should eq 5 # RSpec
+#   10.times { |i| m.b(i) }
+#   assert_equal 5, count # Test::Unit
+#   count.should eq 5 # RSpec
 #
 module MicroMock
 
